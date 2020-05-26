0c42f8e017d5fc060516850224e0e8d01c987e2d
Lakshay Dua <lakshaydua08@gmail.com>
2020-05-26 23:06:57 +0530

    Updates

---
+++ precache-manifest.f7b5c20382f81f36736eeb7415fd35a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b674e05bd9f7112a048c8a4c629eb761",
+    "url": "/lakshayD08/tic-tac-toe.git/index.html"
+  },
+  {
+    "revision": "387632eb48dff4550370",
+    "url": "/lakshayD08/tic-tac-toe.git/static/css/main.996b4e28.chunk.css"
+  },
+  {
+    "revision": "3cf2d9dda7cf9f4ce427",
+    "url": "/lakshayD08/tic-tac-toe.git/static/js/2.a5191be2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lakshayD08/tic-tac-toe.git/static/js/2.a5191be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387632eb48dff4550370",
+    "url": "/lakshayD08/tic-tac-toe.git/static/js/main.c05b9682.chunk.js"
+  },
+  {
+    "revision": "65c82998883bba951bdb",
+    "url": "/lakshayD08/tic-tac-toe.git/static/js/runtime-main.75f3d999.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lakshayD08/tic-tac-toe.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
